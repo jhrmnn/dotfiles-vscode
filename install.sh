183aa3c33df45c107dc8acad7ca723d1cf06b1cc
Jan Hermann <github@id.jan.hermann.name>
2023-02-12 00:39:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 [[ ! -f ~/.profile ]] || mv ~/.profile ~/.profile.bkp
 [[ ! -f ~/.bashrc ]] || mv ~/.bashrc ~/.bashrc.bkp
 yadm clone --no-bootstrap https://github.com/jhrmnn/dotfiles.git
-echo XXX
+yadm reset --hard HEAD
